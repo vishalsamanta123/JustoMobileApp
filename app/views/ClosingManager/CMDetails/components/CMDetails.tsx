95b9e29d386e24334774019d7f6c10143a47d751
amanpatidar1 <amanpatidar@itinformatix.com>
2022-12-01 17:07:11 +0530

    closing model changes and add

---
+++ app/views/ClosingManager/CMDetails/components/CMDetails.tsx
@@ -0,0 +1,150 @@
+import React, { useEffect, useState } from 'react';
+import { View, Text, useWindowDimensions, Image } from 'react-native';
+import images from '../../../../assets/images';
+import Header from '../../../../components/Header';
+import { PRIMARY_THEME_COLOR, PRIMARY_THEME_COLOR_DARK, TABBAR_COLOR } from '../../../../components/utilities/constant';
+import strings from '../../../../components/utilities/Localization';
+import styles from './styles';
+import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import CMInfoView from './CMInfo';
+import StatsView from './StatsViews';
+import Button from '../../../../components/Button';
+import { useSelector } from 'react-redux';
+
+const CMDetailsView = (props: any) => {
+    const [SMdetail, setSMdetail] = useState([])
+    const { response = {}, detail = '' } = useSelector((state: any) => state.SourcingManager)
+    useEffect(() => {
+        if (response && response?.status === 200) {
+            if (response?.data?.length > 0) {
+                setSMdetail(response?.data[0] ? response?.data[0] : []);
+            }
+        } else {
+            setSMdetail([]);
+            //errorToast(response.message);
+        }
+    }, [response])
+    const DATAINFO: any =
+    {
+        AgentName: 'ABC',
+        Mobileno: '12586663',
+        Email: 'Abc@gmail.com',
+        whatsappno: '8890898779',
+        rerano: '12345699',
+        aadharno: '12345699',
+        pancardno: 'AAAAA2225A',
+        gst: 'ABC123',
+        workingfrom: '22/10/2021',
+        workinglocation: ['indoe', 'Dewash'],
+        allocatedCp: [
+            {
+                id: 1,
+                cpName: 'CP 1'
+            },
+            {
+                id: 2,
+                cpName: 'CP 2'
+            },
+            {
+                id: 3,
+                cpName: 'CP 3'
+            },
+            {
+                id: 4,
+                cpName: 'CP 4'
+            },
+        ]
+    };
+    const DATASTATS: any =
+    {
+        closingper: 10,
+        visitor: 123,
+        siteVisit: 234,
+        closeVisit: 600,
+        lastlogin: '2 min ago',
+        lastvisit: '2 min ago',
+        lastsitevisit: '2 min ago',
+        lastclosevisit: '2 min ago',
+        month: 'March',
+        startDate: '01/02/2022',
+        endDate: '10/02/2022',
+        visitTarget: '10/02',
+        siteVisitTarget: '15/02',
+        closeTarget: '12/02',
+    };
+
+
+    const layout = useWindowDimensions();
+
+    const [index, setIndex] = useState(0);
+    const [routes] = useState([
+        { key: 'first', title: 'Stats' },
+        { key: 'second', title: 'SM Info' },
+    ]);
+
+    const renderTabBar = (props: any) => (
+        <TabBar
+            activeColor={TABBAR_COLOR}
+            //inactiveColor={'#F4F4F4'} 
+            {...props}
+            indicatorStyle={{ borderWidth: 2, borderColor: TABBAR_COLOR }}
+            style={{ backgroundColor: PRIMARY_THEME_COLOR_DARK }} />
+    );
+    const FirstRoute = () => (
+        <StatsView items={SMdetail} handleCpAllocation={props.handleCpAllocationPress} />
+    );
+
+    const SecondRoute = () => (
+        <CMInfoView items={SMdetail} handleCpAllocation={props.handleCpAllocationPress} />
+    );
+    const renderScene = SceneMap({
+        first: FirstRoute,
+        second: SecondRoute,
+    });
+    return (
+        <View style={styles.mainContainer}>
+            <Header
+                leftImageSrc={images.backArrow}
+                rightSecondImageScr={images.notification}
+                headerText={strings.SMDetails}
+                headerStyle={styles.headerStyle}
+                RightFirstIconStyle={styles.leftImageIconStyle}
+                leftImageIconStyle={styles.leftImageIconStyle}
+                handleOnLeftIconPress={props.handleBackPress}
+                statusBarColor={PRIMARY_THEME_COLOR}
+                barStyle={'light-content'}
+            />
+            <View style={styles.topItemVw}>
+                <Image
+                    source={images.chat}
+                    style={styles.topItemSty}
+                />
+                <Image
+                    source={images.chat}
+                    style={styles.topItemSty}
+                />
+            </View>
+            <View style={styles.propertyListView}>
+                <TabView
+                    renderTabBar={renderTabBar}
+                    navigationState={{ index, routes }}
+                    renderScene={renderScene}
+                    onIndexChange={setIndex}
+                    initialLayout={{ width: layout.width }}
+                />
+            </View>
+            <View style={{ marginVertical: 12, alignItems: 'center', }}>
+                <Button
+                    width={150}
+                    height={40}
+                    btnTxtsize={16}
+                    textTransform={null}
+                    buttonText={strings.cpAllocation}
+                    handleBtnPress={() => props.handleCpAllocationPress()}
+                />
+            </View>
+        </View>
+    )
+}
+export default CMDetailsView
---
+++ app/views/ClosingManager/CMDetails/components/StatsViews.tsx
@@ -0,0 +1,160 @@
+import moment from "moment";
+import React from "react";
+import { View, Text } from 'react-native';
+import { ScrollView } from "react-native-gesture-handler";
+import Button from "../../../../components/Button";
+import { normalize } from "../../../../components/scaleFontSize";
+import { BLACK_COLOR } from "../../../../components/utilities/constant";
+import strings from "../../../../components/utilities/Localization";
+import styles from "./styles";
+
+const StatsView = (props: any) => {
+    const item = props?.items || {}
+    const current_target = item?.current_target?.length > 0 ? item?.current_target?.[0] : []
+    return (
+        <ScrollView>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>No. of site visit</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{item?.user_states?.total_visit}</Text>
+                </View>
+            </View>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>No. of Close visit</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{item?.user_states?.total_closing_lead}</Text>
+                </View>
+            </View>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Closing Persentage</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{item?.user_states?.total_closing_percentage}</Text>
+                </View>
+            </View>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Last Login</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{moment(item?.user_states?.last_login).format('DD-MM-YYYY')}</Text>
+                </View>
+            </View>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Last Lead Create </Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{item?.user_states?.last_visit}</Text>
+                </View>
+            </View>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Last site visit</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{item?.user_states?.last_site_visit}</Text>
+                </View>
+            </View>
+            <View style={styles.Txtview}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Last close visit</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{item?.user_states?.last_closing_lead}</Text>
+                </View>
+            </View>
+            <Text style={styles.bigTitlesTxt}>Current Target</Text>
+            <View style={[styles.Txtview, { borderTopWidth: 1 }]}>
+                <View><Text>:</Text></View>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Month</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>
+                        {
+                            current_target?.month
+                        }
+                    </Text>
+                </View>
+            </View>
+            <View style={[styles.Txtview, { borderTopWidth: 1 }]}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Start Date</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{moment(current_target?.start_date).format('DD-MM-YYYY')}</Text>
+                </View>
+            </View>
+            <View style={[styles.Txtview, { borderTopWidth: 1 }]}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>End Date</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>{moment(current_target?.end_date).format('DD-MM-YYYY')}</Text>
+                </View>
+            </View>
+            <View style={[styles.Txtview, { borderTopWidth: 1 }]}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Visit target</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>
+                        {current_target?.visitTarget ?
+
+                            `${current_target?.visitTarget} / ${current_target?.achieve_visit_target}`
+                            : null
+                        }
+                    </Text>
+                </View>
+            </View>
+            <View style={[styles.Txtview, { borderTopWidth: 1 }]}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Site visit target</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>
+                        {current_target?.site_visit_target ?
+
+                            `${current_target?.site_visit_target} / ${current_target?.achieve_site_visit_target}`
+                            : null
+                        }
+                    </Text>
+                </View>
+            </View>
+            <View style={[styles.Txtview, { borderTopWidth: 1 }]}>
+                <View style={styles.projectContainer}>
+                    <Text style={styles.projectTxt}>Close target</Text>
+                </View>
+                <View><Text>:</Text></View>
+                <View style={styles.nameContainer}>
+                    <Text style={styles.nameTxt}>
+                        {current_target?.closing_target ?
+
+                            `${current_target?.closing_target} / ${current_target?.achieve_closing_target}`
+                            : null
+                        }
+                    </Text>
+                </View>
+            </View>
+        </ScrollView>
+    )
+}
+export default StatsView
