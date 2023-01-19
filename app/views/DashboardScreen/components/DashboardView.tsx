1ad53f7b306cd9883f4757ab8117bd5b16715c51
NimishDubey <nimish.dubey@itinformatix.com>
2023-01-19 12:13:00 +0530

    dashboard navigation

--- app/views/DashboardScreen/components/DashboardView.tsx
+++ app/views/DashboardScreen/components/DashboardView.tsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
-import { View, Text, ScrollView, Image, FlatList, TouchableOpacity, } from "react-native";
-import { useSafeAreaInsets } from 'react-native-safe-area-context';
-import { Switch } from 'react-native-switch';
+import {
+  View,
+  Text,
+  ScrollView,
+  Image,
+  FlatList,
+  TouchableOpacity,
+} from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Switch } from "react-native-switch";
 import Header from "../../../components/Header";
 import images from "../../../assets/images";
 import styles from "./styles";
 import strings from "../../../components/utilities/Localization";
-import { GREEN_COLOR, RED_COLOR, ROLE_IDS, WHITE_COLOR } from "../../../components/utilities/constant";
+import {
+  GREEN_COLOR,
+  RED_COLOR,
+  ROLE_IDS,
+  WHITE_COLOR,
+} from "../../../components/utilities/constant";
 import SourcingDashboardView from "./SourcingView";
 import ClosingDashboardView from "./ClosingView";
 import ComingSoonScreen from "app/components/CommonScreen/ComingSoon";
@@ -14,14 +26,29 @@ import PostSaleDashboardView from "./PostSalesView";
 import ReceiptionistDashboardView from "./ReceptionistView";
 
 const DashboardView = (props: any) => {
-  const roleType = props?.getLoginType?.response?.data?.role_id || null
+  const roleType = props?.getLoginType?.response?.data?.role_id || null;
   const renderItem = ({ item }: any) => {
     return (
-      <TouchableOpacity style={styles.headingView}>
-        <Text style={styles.itemText}>{
-          roleType === ROLE_IDS.sourcingtl_id ?
-            item.user_name : roleType === ROLE_IDS.sourcingmanager_id
-              ? item.agent_name : strings.notfount}</Text>
+      <TouchableOpacity
+        style={styles.headingView}
+        onPress={() =>
+          props.handleItemNavigateToDetail(
+            roleType === ROLE_IDS.sourcingtl_id
+              ? "TL"
+              : roleType === ROLE_IDS.sourcingmanager_id
+              ? "SM"
+              : "ANY",
+            item
+          )
+        }
+      >
+        <Text style={styles.itemText}>
+          {roleType === ROLE_IDS.sourcingtl_id
+            ? item.user_name
+            : roleType === ROLE_IDS.sourcingmanager_id
+            ? item.agent_name
+            : strings.notfount}
+        </Text>
         <Text style={styles.itemText}>{item.total_visit}</Text>
         <Text style={styles.itemText}>{item.total_site_visit}</Text>
         <Text style={styles.itemText}>{item.total_closing_lead}</Text>
@@ -40,8 +67,11 @@ const DashboardView = (props: any) => {
           handleOnLeftIconPress={props.handleDrawerPress}
           headerStyle={styles.headerStyle}
         />
-        <ScrollView showsVerticalScrollIndicator={false}
-          contentContainerStyle={styles.dashboardScroll} bounces={false}>
+        <ScrollView
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={styles.dashboardScroll}
+          bounces={false}
+        >
           <View style={styles.dashboardWrap}>
             <View style={styles.nameView}>
               <View style={styles.statusView}>
@@ -49,15 +79,17 @@ const DashboardView = (props: any) => {
                 <View style={styles.switchView}>
                   <Switch
                     value={props?.isEnabled === 1 ? true : false}
-                    onValueChange={(val) => props.updateStatusPress(props?.isEnabled)}
+                    onValueChange={(val) =>
+                      props.updateStatusPress(props?.isEnabled)
+                    }
                     //disabled={false}
                     backgroundActive={GREEN_COLOR}
                     backgroundInactive={RED_COLOR}
                     circleActiveColor={WHITE_COLOR}
                     circleInActiveColor={WHITE_COLOR}
                     circleSize={25}
-                    activeText={''}
-                    inActiveText={''}
+                    activeText={""}
+                    inActiveText={""}
                     // barHeight={1}
                     circleBorderWidth={2}
                   />
@@ -65,92 +97,103 @@ const DashboardView = (props: any) => {
               </View>
               <View style={styles.welcomeView}>
                 <Text style={styles.welcomeToText}>Welcome</Text>
-                <Text style={styles.welcomeNameText}>{props?.dashboardData?.user_name}</Text>
+                <Text style={styles.welcomeNameText}>
+                  {props?.dashboardData?.user_name}
+                </Text>
               </View>
             </View>
             <View style={styles.qrCodeView}>
-              {props?.dashboardData?.qrcode != "" || props?.dashboardData?.qr_code ?
-                <Image source={{ uri: props?.dashboardData?.qrcode || props?.dashboardData?.qr_code }}
-                  style={styles.qrCodeImage} />
-                :
+              {props?.dashboardData?.qrcode != "" ||
+              props?.dashboardData?.qr_code ? (
+                <Image
+                  source={{
+                    uri:
+                      props?.dashboardData?.qrcode ||
+                      props?.dashboardData?.qr_code,
+                  }}
+                  style={styles.qrCodeImage}
+                />
+              ) : (
                 <Image source={images.qrCode} style={styles.qrCodeImage} />
-              }
+              )}
               {/* <TouchableOpacity style={styles.linkImageView}>
                 <Image source={images.link} style={styles.linkImage} />
               </TouchableOpacity> */}
             </View>
           </View>
           {roleType === ROLE_IDS.sourcingtl_id ||
-            roleType === ROLE_IDS.sourcingmanager_id ?
+          roleType === ROLE_IDS.sourcingmanager_id ? (
             <SourcingDashboardView
               dashboardData={props?.dashboardData}
               getLoginType={props.getLoginType}
             />
-            :
+          ) : (
             <>
               {roleType === ROLE_IDS.closingtl_id ||
-                roleType === ROLE_IDS.closingmanager_id ?
+              roleType === ROLE_IDS.closingmanager_id ? (
                 <ClosingDashboardView
                   dashboardData={props?.dashboardData}
                   getLoginType={props.getLoginType}
-                /> :
+                />
+              ) : (
                 <>
-                  {roleType === ROLE_IDS.postsales_id ?
+                  {roleType === ROLE_IDS.postsales_id ? (
                     <PostSaleDashboardView
                       dashboardData={props?.dashboardData}
                       getLoginType={props.getLoginType}
-                    /> :
+                    />
+                  ) : (
                     <>
-                      {roleType === ROLE_IDS.receptionist_id ?
-                        <ReceiptionistDashboardView dashboardData={props?.dashboardData} /> :
+                      {roleType === ROLE_IDS.receptionist_id ? (
+                        <ReceiptionistDashboardView
+                          dashboardData={props?.dashboardData}
+                        />
+                      ) : (
                         <View style={styles.secondPortion}>
                           <ComingSoonScreen />
                         </View>
-                      }
+                      )}
                     </>
-                  }
+                  )}
                 </>
-              }
+              )}
             </>
-          }
-          {props?.listData?.length > 0 ?
-            (<View style={styles.bottomSection}>
+          )}
+          {props?.listData?.length > 0 ? (
+            <View style={styles.bottomSection}>
               <View style={styles.headingView}>
-                {roleType === ROLE_IDS.sourcingtl_id ?
+                {roleType === ROLE_IDS.sourcingtl_id ? (
                   <>
                     <Text style={styles.headingText}>SM NAME</Text>
                     <Text style={styles.headingText}>VISITOR</Text>
                     <Text style={styles.headingText}>SITE VISIT</Text>
                     <Text style={styles.headingText}>CLOSE LEAD</Text>
                   </>
-                  :
+                ) : (
                   <>
-                    {roleType === ROLE_IDS.sourcingmanager_id &&
+                    {roleType === ROLE_IDS.sourcingmanager_id && (
                       <>
                         <Text style={styles.headingText}>CP NAME</Text>
                         <Text style={styles.headingText}>VISITOR</Text>
                         <Text style={styles.headingText}>SITE VISIT</Text>
                         <Text style={styles.headingText}>CLOSE LEAD</Text>
                       </>
-                    }
+                    )}
                   </>
-                }
+                )}
               </View>
-              <FlatList
-                data={props?.listData}
-                renderItem={renderItem} />
+              <FlatList data={props?.listData} renderItem={renderItem} />
               {roleType === ROLE_IDS.sourcingtl_id ||
-                roleType === ROLE_IDS.sourcingmanager_id
-                && props?.listData?.length > 5 ?
+              (roleType === ROLE_IDS.sourcingmanager_id &&
+                props?.listData?.length > 5) ? (
                 <TouchableOpacity style={styles.headingView}>
                   <Text style={[styles.headingText, styles.knowMoreText]}>
                     Know More
                   </Text>
-                </TouchableOpacity> : null
-              }
-            </View>)
-            : null
-          }
+                </TouchableOpacity>
+              ) : null}
+            </View>
+          ) : null}
         </ScrollView>
       </View>
     </>
--- app/views/DashboardScreen/index.tsx
+++ app/views/DashboardScreen/index.tsx
@@ -1,98 +1,139 @@
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
-import ErrorMessage from 'app/components/ErrorMessage';
-import { GREEN_COLOR, ROLE_IDS } from 'app/components/utilities/constant';
-import { getAllAgentList } from 'app/Redux/Actions/AgencyActions';
-import { dashboardClosingData, dashboardPostSaleData, dashboardReceptionistData, dashboardSourcingData, userStatusUpdateData, userStatusUpdater } from 'app/Redux/Actions/Dashboard';
-import { getAssignCPList, getSourcingManagerList } from 'app/Redux/Actions/SourcingManagerActions';
-import React, { useEffect, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import DashboardView from './components/DashboardView';
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useFocusEffect } from "@react-navigation/native";
+import ErrorMessage from "app/components/ErrorMessage";
+import { GREEN_COLOR, ROLE_IDS } from "app/components/utilities/constant";
+import { getAllAgentList } from "app/Redux/Actions/AgencyActions";
+import {
+  dashboardClosingData,
+  dashboardPostSaleData,
+  dashboardReceptionistData,
+  dashboardSourcingData,
+  userStatusUpdateData,
+  userStatusUpdater,
+} from "app/Redux/Actions/Dashboard";
+import {
+  getAssignCPList,
+  getSourcingManagerList,
+} from "app/Redux/Actions/SourcingManagerActions";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import DashboardView from "./components/DashboardView";
 
 const DashboardScreen = ({ navigation }: any) => {
-  const dispatch: any = useDispatch()
+  const dispatch: any = useDispatch();
   const getLoginType = useSelector((state: any) => state.login);
-  const statusData = useSelector((state: any) => state.statusUpdateData) || {}
-  const { response = {}, } = useSelector((state: any) => state.dashboardData);
-  const SMListData = useSelector((state: any) => state.SourcingManager)
-  const [dashboardData, setDashboardData] = useState({})
-  const [listData, setListData] = useState<any>([])
+  const statusData = useSelector((state: any) => state.statusUpdateData) || {};
+  const { response = {} } = useSelector((state: any) => state.dashboardData);
+  const SMListData = useSelector((state: any) => state.SourcingManager);
+  const [dashboardData, setDashboardData] = useState({});
+  const [listData, setListData] = useState<any>([]);
   const [isEnabled, setIsEnabled] = useState<any>();
 
   useFocusEffect(
     React.useCallback(() => {
-      getDashboard()
-      return () => { };
+      getDashboard();
+      return () => {};
     }, [navigation, isEnabled, getLoginType])
   );
   useEffect(() => {
     if (response?.status === 200) {
-      setDashboardData(response?.data)
-      setIsEnabled(response?.data?.online_status)
+      setDashboardData(response?.data);
+      setIsEnabled(response?.data?.online_status);
     } else {
-      setDashboardData({})
-      setIsEnabled(null)
+      setDashboardData({});
+      setIsEnabled(null);
     }
-    if (getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingtl_id ||
-      getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingmanager_id) {
+    if (
+      getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingtl_id ||
+      getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingmanager_id
+    ) {
       if (SMListData?.response?.status === 200) {
-        setListData(SMListData?.response?.data)
+        setListData(SMListData?.response?.data);
       } else {
-        setListData([])
+        setListData([]);
       }
     }
-  }, [response, SMListData])
+  }, [response, SMListData]);
   const getDashboard = async () => {
-    if (getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingtl_id ||
-      getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingmanager_id) {
-      dispatch(dashboardSourcingData({}))
+    if (
+      getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingtl_id ||
+      getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingmanager_id
+    ) {
+      dispatch(dashboardSourcingData({}));
       if (getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingtl_id) {
-        dispatch(getSourcingManagerList({}))
-      } else if (getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingmanager_id) {
-        dispatch(getAssignCPList({
-          user_id: getLoginType?.response?.data?.user_id,
-        }))
+        dispatch(getSourcingManagerList({}));
+      } else if (
+        getLoginType?.response?.data?.role_id === ROLE_IDS.sourcingmanager_id
+      ) {
+        dispatch(
+          getAssignCPList({
+            user_id: getLoginType?.response?.data?.user_id,
+          })
+        );
       } else {
-        setListData([])
+        setListData([]);
       }
-    } else if (getLoginType?.response?.data?.role_id === ROLE_IDS.closingtl_id ||
-      getLoginType?.response?.data?.role_id === ROLE_IDS.closingmanager_id) {
-      dispatch(dashboardClosingData({}))
-    } else if (getLoginType?.response?.data?.role_id === ROLE_IDS.postsales_id) {
-      dispatch(dashboardPostSaleData({}))
-    } else if (getLoginType?.response?.data?.role_id === ROLE_IDS.receptionist_id) {
-      dispatch(dashboardReceptionistData({}))
+    } else if (
+      getLoginType?.response?.data?.role_id === ROLE_IDS.closingtl_id ||
+      getLoginType?.response?.data?.role_id === ROLE_IDS.closingmanager_id
+    ) {
+      dispatch(dashboardClosingData({}));
+    } else if (
+      getLoginType?.response?.data?.role_id === ROLE_IDS.postsales_id
+    ) {
+      dispatch(dashboardPostSaleData({}));
+    } else if (
+      getLoginType?.response?.data?.role_id === ROLE_IDS.receptionist_id
+    ) {
+      dispatch(dashboardReceptionistData({}));
     } else {
-      setDashboardData({})
-      setIsEnabled(null)
+      setDashboardData({});
+      setIsEnabled(null);
     }
-  }
+  };
   const updateStatusPress = (data: any) => {
-    dispatch(userStatusUpdateData({
-      online_status: data === 1 ? 2 : 1
-    }))
-  }
+    dispatch(
+      userStatusUpdateData({
+        online_status: data === 1 ? 2 : 1,
+      })
+    );
+  };
   useEffect(() => {
     if (statusData?.data && statusData?.response?.status === 200) {
-      setIsEnabled(isEnabled === 1 ? 2 : 1)
-      dispatch(userStatusUpdater())
+      setIsEnabled(isEnabled === 1 ? 2 : 1);
+      dispatch(userStatusUpdater());
       ErrorMessage({
         msg: statusData?.response?.message,
-        backgroundColor: GREEN_COLOR
-      })
+        backgroundColor: GREEN_COLOR,
+      });
     }
-  }, [statusData])
+  }, [statusData]);
   const handleDrawerPress = () => {
-    navigation.toggleDrawer()
+    navigation.toggleDrawer();
   };
-  return <DashboardView
-    dashboardData={dashboardData}
-    handleDrawerPress={handleDrawerPress}
-    updateStatusPress={updateStatusPress}
-    isEnabled={isEnabled}
-    listData={listData}
-    getLoginType={getLoginType}
-  />;
+  const handleKnowMorePress = () => {};
+
+  const handleItemNavigateToDetail = (type: any, data: any) => {
+    console.log("type: ", type);
+    console.log("data: ", data);
+    if (type === "TL") {
+      navigation.navigate('SMDetails', data)
+
+    } else if (type === "SM") {
+      navigation.navigate("AgencyDetails", { data });
+    }
+  };
+  return (
+    <DashboardView
+      dashboardData={dashboardData}
+      handleDrawerPress={handleDrawerPress}
+      updateStatusPress={updateStatusPress}
+      isEnabled={isEnabled}
+      listData={listData}
+      getLoginType={getLoginType}
+      handleItemNavigateToDetail={handleItemNavigateToDetail}
+    />
+  );
 };
 
 export default DashboardScreen;
